beda38c16d095fd64f71f257965889013f6fa281
Satyam <satyam@satyam.com.ar>
2020-09-15 19:10:16 +0200

    Before switching from C to A paths

--- src/Teatro.js
+++ src/Teatro.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import RO from './ro';
 
 const STEPS = 20;
 const INTERVAL = 200;
@@ -37,71 +38,72 @@ function Teatro() {
     // console.log('c', c, i, step);
     return Math.round(args[i] + (args[i + 1] - args[i]) * c);
   };
-  const q = `${p(60, 60, 40)},${p(40, 50, 60)}`;
-  const q1 = `${p(60, 46, 40)},${p(54, 50, 46)}`;
-  const l = `${p(60, 80, 40)},${p(20, 50, 80)}`;
+  return <RO step={step} p={p} />;
+  // const q = `${p(60, 60, 40)},${p(40, 50, 60)}`;
+  // const q1 = `${p(60, 46, 40)},${p(54, 50, 46)}`;
+  // const l = `${p(60, 80, 40)},${p(20, 50, 80)}`;
 
-  const d = `M 40,20 L 40,40 C 40,54 ${q1} ${q} L ${l}`;
+  // const d = `M 40,20 L 40,40 C 40,54 ${q1} ${q} L ${l}`;
   // console.log(step, up, d);
-  return (
-    <svg viewBox={`0 0 100 100`} xmlns="http://www.w3.org/2000/svg" width="60%">
-      <path strokeWidth={1} fill="none" stroke="black" d={d} />
-      <circle
-        strokeWidth={1}
-        fill="none"
-        stroke="blue"
-        cx="50"
-        cy="40"
-        r="10"
-      />{' '}
-      <path
-        strokeWidth={1}
-        fill="none"
-        stroke="gray"
-        d="M20,20 L20,40 C 20,80 80,80 80,40 L 80,20"
-      />
-      <circle
-        strokeWidth={1}
-        fill="none"
-        stroke="blue"
-        cx="50"
-        cy="40"
-        r="30"
-      />{' '}
-      <circle
-        strokeWidth={1}
-        fill="green"
-        stroke="green"
-        cx={p(60, 60, 40)}
-        cy={p(40, 50, 60)}
-        r="2"
-      />
-      <circle
-        strokeWidth={1}
-        fill="silver"
-        stroke="silver"
-        cx={40}
-        cy={54}
-        r="2"
-      />
-      <circle
-        strokeWidth={1}
-        fill="yellow"
-        stroke="yellow"
-        cx={p(60, 46, 40)}
-        cy={p(54, 50, 46)}
-        r="2"
-      />
-      <circle
-        strokeWidth={1}
-        fill="red"
-        stroke="red"
-        cx={p(60, 80, 40)}
-        cy={p(20, 50, 80)}
-        r="2"
-      />
-    </svg>
-  );
+  // return (
+  //   <svg viewBox={`0 0 100 100`} xmlns="http://www.w3.org/2000/svg" width="60%">
+  //     <path strokeWidth={1} fill="none" stroke="black" d={d} />
+  //     <circle
+  //       strokeWidth={1}
+  //       fill="none"
+  //       stroke="blue"
+  //       cx="50"
+  //       cy="40"
+  //       r="10"
+  //     />{' '}
+  //     <path
+  //       strokeWidth={1}
+  //       fill="none"
+  //       stroke="gray"
+  //       d="M20,20 L20,40 C 20,80 80,80 80,40 L 80,20"
+  //     />
+  //     <circle
+  //       strokeWidth={1}
+  //       fill="none"
+  //       stroke="blue"
+  //       cx="50"
+  //       cy="40"
+  //       r="30"
+  //     />{' '}
+  //     <circle
+  //       strokeWidth={1}
+  //       fill="green"
+  //       stroke="green"
+  //       cx={p(60, 60, 40)}
+  //       cy={p(40, 50, 60)}
+  //       r="2"
+  //     />
+  //     <circle
+  //       strokeWidth={1}
+  //       fill="silver"
+  //       stroke="silver"
+  //       cx={40}
+  //       cy={54}
+  //       r="2"
+  //     />
+  //     <circle
+  //       strokeWidth={1}
+  //       fill="yellow"
+  //       stroke="yellow"
+  //       cx={p(60, 46, 40)}
+  //       cy={p(54, 50, 46)}
+  //       r="2"
+  //     />
+  //     <circle
+  //       strokeWidth={1}
+  //       fill="red"
+  //       stroke="red"
+  //       cx={p(60, 80, 40)}
+  //       cy={p(20, 50, 80)}
+  //       r="2"
+  //     />
+  //   </svg>
+  // );
 }
 
 export default Teatro;
