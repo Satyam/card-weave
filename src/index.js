aaaeb4a4cdb5228a0ce3427dadc697e2e83cd63e
Satyam <satyam@satyam.com.ar>
2020-09-20 20:08:43 +0200

    Added file for printout

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import Teatro from './Teatro';
-
+import Zoo from './zoo';
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
     <Teatro />
+    <Zoo />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/zoo.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import U from './u';
+import RO from './ro';
+import OX from './ox';
+import XY from './xy';
+import YR from './yr';
+
+const STEPS = 20;
+
+export default function Zoo() {
+  const step = 1;
+  const p = (...args) => {
+    const l = args.length;
+    const f = (step * (l - 1)) / STEPS;
+    const i = Math.min(l - 2, Math.floor(f));
+    const c = f - i;
+    // console.log(step, { l, f, i, c });
+    return Math.round(args[i] + (args[i + 1] - args[i]) * c);
+  };
+  const args = {
+    fill: `rgb(${p(255, 219)}, ${p(35, 0)}, ${p(8, 154)})`,
+    backColor: 'white',
+    width: '25%',
+    // transform: `
+    // translate(${-counter * 5}, ${-counter * 5}),
+    // scale(${(20 - counter) / 20}),
+    // rotate(${counter * 5}, 300, 300)
+    // `,
+  };
+  return (
+    <>
+      <U />
+      {new Array(4).fill(1).map((_, row) => (
+        <div key={row} style={{ width: '15cm' }}>
+          {new Array(4).fill(1).map((_, i) => {
+            const args1 = { ...args, p: (...a) => a[i] };
+            const C = [RO, OX, XY, YR][row];
+            return <C key={i} {...args1} />;
+          })}
+        </div>
+      ))}
+    </>
+  );
+}
