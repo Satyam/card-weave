e13bdb1087c7024fa036583477e1812107336c8e
Satyam <satyam@satyam.com.ar>
2020-09-21 19:32:15 +0200

    Adde twisting shadow effect

--- src/zoo.jsx
+++ src/zoo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import U from './u';
+import Border from './border';
 import RO from './ro';
 import OX from './ox';
 import XY from './xy';
@@ -8,50 +9,43 @@ import YR from './yr';
 
 import './zoo.css';
 
-const STEPS = 20;
-
 const map = (n, fn) => new Array(n).fill(1).map((_, index) => fn(index));
 
 export default function Zoo() {
-  const step = 1;
-  const p = (...args) => {
-    const l = args.length;
-    const f = (step * (l - 1)) / STEPS;
-    const i = Math.min(l - 2, Math.floor(f));
-    const c = f - i;
-    // console.log(step, { l, f, i, c });
-    return Math.round(args[i] + (args[i + 1] - args[i]) * c);
-  };
-  const args = {
-    backColor: 'white',
-    width: '25%',
-    // transform: `
-    // translate(${-counter * 5}, ${-counter * 5}),
-    // scale(${(20 - counter) / 20}),
-    // rotate(${counter * 5}, 300, 300)
-    // `,
-  };
   const colors = map(8, (i) => {
     const p = (a, b) => Math.round(a + ((b - a) * i) / 7);
     return `rgb(${p(255, 219)}, ${p(35, 0)}, ${p(8, 154)})`;
   });
   colors.forEach((c, i) => (colors[15 - i] = c));
 
+  const D = 4;
   return (
     <>
+      <Border />
       <U />
       {map(4, (row) => (
         <div key={row} className="row">
-          {map(4, (col) => {
-            const args1 = {
-              ...args,
-              p: (...a) => a[col],
-              fill: colors[row * 4 + col],
-            };
-            console.log(row, col, row * 4 + col, colors[row * 4 + col]);
-            const C = [RO, OX, XY, YR][row];
-            return <C key={col} {...args1} />;
-          })}
+          {map(4, (col) => (
+            <svg viewBox={`-100 -100 800 1200`} className="col" key={col}>
+              {map(D, (depth) => {
+                const rD = D - depth;
+                const inv = 1 / rD;
+                const args = {
+                  backColor: 'white',
+                  p: (...a) => a[col],
+                  fill: colors[(row * 4 + col + depth) % 16],
+                  transform: `
+                  translate(${-(rD - 1) * 10}, ${-(rD - 1) * 10}),
+                  scale(${inv}),
+                    rotate(${(rD - 1) * 10}, 300, 300)
+                    `,
+                  fillOpacity: inv,
+                };
+                const C = [RO, OX, XY, YR][row];
+                return <C key={depth} {...args} />;
+              })}
+            </svg>
+          ))}
         </div>
       ))}
     </>
