d31db65edc0cbb8383d35625b5fb75ea46db83a7
Satyam <satyam@satyam.com.ar>
2020-09-22 18:24:26 +0200

    Added spiral for base

---
+++ src/espiral.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import './zoo.css';
+const map = (n, fn) => new Array(n).fill(1).map((_, index) => fn(index));
+
+const INC = 235;
+const RADIO_MAX = 1000;
+const GAJOS = 8;
+const espiral = (cntInicial, cw) => {
+  let r = 0;
+  let antX = 0;
+  let antY = 0;
+  let count = cntInicial;
+  const d = ['M0,0'];
+
+  for (let x = 0, y = 0; r < RADIO_MAX; count = (count + 1) % 4) {
+    r += INC;
+    switch (count) {
+      case 0:
+        antX = x = antX + r;
+        antY = y = antY + r;
+        break;
+      case 1:
+        antX = x = antX + r;
+        antY = y = antY - r;
+        break;
+      case 2:
+        antX = x = antX - r;
+        antY = y = antY - r;
+        break;
+      case 3:
+        antX = x = antX - r;
+        antY = y = antY + r;
+        break;
+      default:
+        break;
+    }
+    d.push(`A ${r}, ${r} 90 0 ${cw ? 1 : 0} ${x}, ${y}`);
+  }
+  return d.join(' ');
+};
+export default function Espiral() {
+  return (
+    <svg
+      viewBox={`-1000 -1000 2000 2000`}
+      xmlns="http://www.w3.org/2000000/svg"
+      className="espiral"
+    >
+      <defs>
+        <path
+          id="E"
+          d={espiral(0, 0)}
+          stroke="blue"
+          fill="none"
+          transform="translate(-50,-50)"
+        />
+      </defs>
+      {map(GAJOS, (a) => (
+        <g>
+          <use href="#E" transform={`rotate(${(360 / GAJOS) * a})`} />
+          <use
+            href="#E"
+            transform={`rotate(${(360 / GAJOS) * a}), scale(-1,1)`}
+          />
+        </g>
+      ))}
+      <circle r={RADIO_MAX} stroke="black" fill="none" strokeWidth={10} />
+    </svg>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 // import App from './App';
 import Teatro from './Teatro';
 import Zoo from './zoo';
+import Espiral from './espiral';
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
     <Teatro />
     <Zoo />
+    <Espiral />
   </React.StrictMode>,
   document.getElementById('root')
 );
