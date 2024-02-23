cff5fa00597022bd98fccb03e29a82bcf5721f72
Satyam <satyam@satyam.com.ar>
2024-02-23 18:54:49 +0100

    Almost finished with new card weave

--- new card weave/index.js
+++ new card weave/index.js
@@ -1,9 +1,19 @@
-const g = document.getElementById('letra');
+/* global Matriz, STORAGE_PREFIX, STATUS */
+const svgEl = document.getElementById('svg');
+const letraPanel = document.getElementById('letra');
+const positionEl = document.getElementById('position');
+const rowsEl = document.getElementById('rows');
+const colsEl = document.getElementById('cols');
+const newBtn = document.getElementById('nuevo');
+const nameEl = document.getElementById('nombre');
+const saveBtn = document.getElementById('guardar');
+const saveAsBtn = document.getElementById('guardarComo');
+const patronesUl = document.getElementById('patrones');
 
-const matriz = new Matriz(17, 10);
+let matriz = null;
 
 function redraw() {
-  g.innerHTML = '';
+  letraPanel.innerHTML = '';
 
   matriz.forEachCell((cell, row, col) => {
     const el = document.createElementNS('http://www.w3.org/2000/svg', 'use');
@@ -12,30 +22,30 @@ function redraw() {
     el.setAttribute('href', `#${col & 1 ? 'O' : 'E'}${cell ? 'N' : 'V'}`);
     el.setAttribute('x', col * 60);
     el.setAttribute('y', row * 30);
-    g.appendChild(el);
+    letraPanel.appendChild(el);
   });
 }
 
-redraw();
-
 function listPatrones() {
-  const ul = document.getElementById('patrones');
   const list = [];
   for (let i = 0; i < localStorage.length; i++) {
     const k = localStorage.key(i);
-    if (k.startsWith('_')) continue;
-    list.push(`<li><a href="${k}">${k}</a></li>`);
+    if (!k.startsWith(STORAGE_PREFIX)) continue;
+    const name = k.replace(STORAGE_PREFIX, '');
+    if (name.startsWith('_')) continue;
+    list.push(`<li><a href="${name}">${name}</a></li>`);
   }
   if (list.length) {
-    ul.innerHTML = list.join('\n');
+    patronesUl.innerHTML = list.join('\n');
   } else {
-    ul.innerHTML = `<li> ---  No hay patrones guardados ---`;
+    patronesUl.innerHTML = `<li> ---  No hay patrones guardados ---`;
   }
 }
 
+// redraw();
 listPatrones();
 
-g.addEventListener('click', (ev) => {
+letraPanel.addEventListener('click', (ev) => {
   const tile = ev.target;
   const row = Number(tile.dataset.row);
   const col = Number(tile.dataset.col);
@@ -44,4 +54,58 @@ g.addEventListener('click', (ev) => {
     'href',
     `#${col & 1 ? 'O' : 'E'}${matriz.matriz[row][col] ? 'N' : 'V'}`
   );
+});
+
+newBtn.addEventListener('click', () => {
+  const rows = Number(rowsEl.value) || 17;
+  const cols = Number(colsEl.value) || 10;
+  svgEl.setAttribute('viewBox', `0,0,${cols * 60},${rows * 30}`);
+  matriz = new Matriz(rows, cols);
+  redraw();
+});
+
+letraPanel.addEventListener('mousemove', (ev) => {
+  const d = ev.target.dataset;
+  positionEl.innerText = `Fila: ${Number(d.row) + 1} Columna: ${
+    Number(d.col) + 1
+  }`;
+});
+
+Matriz.suscribe((m) => {
+  console.log(m.status, m.name);
+  switch (m.status) {
+    case STATUS.NORMAL:
+      saveBtn.disabled = true;
+      saveAsBtn.disabled = false;
+      break;
+    case STATUS.CAMBIADO:
+      saveBtn.disabled = !m.name;
+      saveAsBtn.disabled = false;
+      break;
+    case STATUS.NUEVO:
+      saveBtn.disabled = true;
+      saveAsBtn.disabled = false;
+      break;
+    default:
+      console.error('status desconocido', m.status);
+  }
+  nameEl.value = m.name;
+});
+
+saveBtn.addEventListener('click', () => {
+  matriz.save();
+});
+
+saveAsBtn.addEventListener('click', () => {
+  const name = window.prompt('Ingrese el nuevo nombre', matriz.name);
+  if (name) {
+    matriz.save(name);
+    listPatrones();
+  }
+});
+
+patronesUl.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  matriz = Matriz.read(ev.target.getAttribute('href'));
+  if (matriz) redraw();
 });
--- new card weave/matriz.js
+++ new card weave/matriz.js
@@ -1,7 +1,11 @@
+const STORAGE_PREFIX = 'CW_';
+const STATUS = { NUEVO: 'NUEVO', CAMBIADO: 'CAMBIADO', NORMAL: 'NORMAL' };
+// eslint-disable-next-line no-unused-vars
 class Matriz {
-  constructor(rows = 17, cols = 10) {
-    this.clear(rows, cols);
-    this.name = null;
+  constructor(rows, cols) {
+    if (rows) this.clear(rows, cols);
+    this._name = '';
+    this.status = STATUS.NUEVO;
   }
   clear(rows, cols) {
     this.matriz = Array(rows)
@@ -16,22 +20,52 @@ class Matriz {
   }
   toggle(row, col) {
     this.matriz[row][col] = !this.matriz[row][col];
+    this.status = STATUS.CAMBIADO;
   }
   forEachCell(cb) {
     this.matriz.forEach((line, row) =>
       line.forEach((cell, col) => cb(cell, row, col))
     );
   }
-  read(name) {
-    const m = localStorage.getItem(name);
+  static listeners = [];
+  static suscribe(cb) {
+    if (Matriz.listeners.includes(cb)) return;
+    Matriz.listeners.push(cb);
+  }
+  get name() {
+    return this._name;
+  }
+  set name(n) {
+    if (this._name === n) return;
+    this._name = n;
+    Matriz.listeners.forEach((l) => l(this));
+  }
+  get status() {
+    return this._status;
+  }
+  set status(s) {
+    if (this._status === s) return;
+    this._status = s;
+    Matriz.listeners.forEach((l) => l(this));
+  }
+  static read(name) {
+    const m = localStorage.getItem(STORAGE_PREFIX + name);
     if (m) {
-      this.name = name;
-      this.matriz = JSON.parse(m);
+      const newMatriz = new Matriz();
+      newMatriz.matriz = JSON.parse(m);
+      newMatriz.name = name;
+      newMatriz.status = STATUS.NORMAL;
+      return newMatriz;
     } else {
       alert(`Letra "${name}" no existe en el almacen`);
     }
   }
-  save() {
-    localStorage.setItem(this.name, JSON.stringify(this.matriz));
+  save(newName) {
+    localStorage.setItem(
+      STORAGE_PREFIX + (newName ?? this.name),
+      JSON.stringify(this.matriz)
+    );
+    this.status = STATUS.NORMAL;
+    if (newName) this.name = newName;
   }
 }
